d60136ea3a22b36a742c36f2363996fc6b66c371
mavproductions <markjoudrey78@gmail.com>
2024-02-21 09:50:25 -0400

    - UPDATED: stop.sh copies savednades.json for MatchZy on server shutdown.

--- stop.sh
+++ stop.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo cp -f /home/steam/cs2/game/csgo/cfg/MatchZy/savednades.json /home/steam/cs2/custom_files/cfg/MatchZy/savednades.json
+
 ps -ef | grep install.sh | awk '{print $2}' | xargs kill -9 $1
 ps -ef | grep install.log | awk '{print $2}' | xargs kill -9 $1
 ps -ef | grep gcp.sh | awk '{print $2}' | xargs kill -9 $1
